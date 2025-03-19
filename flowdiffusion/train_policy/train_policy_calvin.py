e53a5ff4ea5a942f2aa04af18ac6a54b83d08586
clemgris <clemence.grislain@eleves.enpc.fr>
2025-03-19 15:47:13 +0100

    Clean code for training diffusion policy

--- flowdiffusion/train_policy/train_policy_calvin.py
+++ flowdiffusion/train_policy/train_policy_calvin.py
@@ -36,14 +36,16 @@ print(f"Total GPUs available: {torch.cuda.device_count()}")
 
 
 def main(args):
-    valid_n = 1
-    sample_per_seq = 8
+    results_folder = "../results_policy_debug/calvin"
 
-    results_folder = "../results_11_03_dino/calvin"
+    if args.server == "jz":
+        data_path = "/lustre/fsn1/projects/rech/fch/uxv44vw/CALVIN/task_D_D"
+    else:
+        data_path = "/home/grislain/AVDC/calvin/dataset/calvin_debug_dataset"
 
     cfg = DictConfig(
         {
-            "root": "/home/grislain/AVDC/calvin/dataset/calvin_debug_dataset",  # "/lustre/fsn1/projects/rech/fch/uxv44vw/CALVIN/task_D_D",
+            "root": data_path,
             "datamodule": {
                 "lang_dataset": {
                     "_target_": "calvin_agent.datasets.disk_dataset.DiskActionDataset",
@@ -51,7 +53,7 @@ def main(args):
                     "save_format": "npz",
                     "batch_size": 32,
                     "min_window_size": 16,
-                    "max_window_size": 64,
+                    "max_window_size": 65,
                     "proprio_state": {
                         "n_state_obs": 8,
                         "keep_indices": [[0, 7], [14, 15]],
@@ -66,13 +68,14 @@ def main(args):
                         "actions": ["actions"],
                         "language": ["language"],
                     },
-                    "skip_frames": 8,
+                    "num_subgoals": 8,
                     "pad": True,
                     "lang_folder": "lang_annotations",
                     "num_workers": 2,
                     "diffuse_on": "pixel",
                 },
             },
+            "save_every": 100,  # In gradient steps
         }
     )
 
@@ -86,78 +89,43 @@ def main(args):
     data_module = CalvinDataModule(
         cfg.datamodule, transforms=transforms, root_data_dir=cfg.root
     )
-    if args.mode == "train":
-        data_module.setup()
+
+    data_module.setup()
     results_folder = Path(results_folder)
 
-    if args.mode == "train":
-        if os.path.exists(results_folder):
-            if not args.override:
-                raise ValueError(
-                    f"Results folder {results_folder} already exists. Use --override to overwrite."
-                )
-        results_folder.mkdir(exist_ok=True, parents=True)
+    if os.path.exists(results_folder):
+        if not args.override:
+            raise ValueError(
+                f"Results folder {results_folder} already exists. Use --override to overwrite."
+            )
+    results_folder.mkdir(exist_ok=True, parents=True)
 
-    with open(os.path.join(results_folder, "data_config.yaml"), "w") as file:
-        file.write(OmegaConf.to_yaml(cfg))
+    train_set = data_module.train_datasets["lang"]
+    valid_set = data_module.val_datasets["lang"]
 
-    if args.mode == "inference":
-        train_set = valid_set = [None]  # dummy
-    else:
-        train_set = data_module.train_datasets["lang"]
-        valid_set = data_module.val_datasets["lang"]
+    print("Train data:", len(train_set))
+    print("Valid data:", len(valid_set))
 
-        print("Train data:", len(train_set))
-        print("Valid data:", len(valid_set))
-
-        ## DEBUG
-        # breakpoint()
-        # for idx in range(len(train_set)):
-        #     start, actions, end = train_set[idx]
-        #     torchvision.utils.save_image(
-        #         start, f"train_start_{idx}.png"
-        #     )
-        #     torchvision.utils.save_image(
-        #         end, f"train_end_{idx}.png"
-        #     )
-        #     if idx > 10: break
-
-        # for idx in range(len(valid_set)):
-        #     x, x_cond, task = valid_set[idx]
-        #     torchvision.utils.save_image(
-        #         start, f"valid_start_{idx}.png"
-        #     )
-        #     torchvision.utils.save_image(
-        #         end, f"valid_end_{idx}.png"
-        #     )
-        #     if idx > 10: break
-        # breakpoint()
-
-    # Create a directory to store the training checkpoint.
-    output_directory = Path("outputs/train/example_pusht_diffusion")
-    output_directory.mkdir(parents=True, exist_ok=True)
-
-    # Number of offline training steps (we'll only do offline training for this example.)
-    # Adjust as you prefer. 5000 steps are needed to get something worth evaluating.
-    training_steps = 5000
+    training_steps = 5000  # TO BE MODIFIED
     device = torch.device("cuda")
-    log_freq = 250
+    log_freq = 10
 
-    # Set up the the policy.
-    # Policies are initialized with a configuration class, in this case `DiffusionConfig`.
-    # For this example, no arguments need to be passed because the defaults are set up for PushT.
-    # If you're doing something different, you will likely need to change at least some of the defaults.
-    diff_cfg = DiffusionConfig(
-        n_obs_steps=2,
-        horizon=8,
-        input_shapes={
-            "observation.image": [3, 96, 96],
-            "observation.state": [0],
-        },
-        output_shapes={
-            "action": [7],
-        },
+    diff_cfg = DictConfig(
+        {
+            "n_obs_steps": 2,
+            "horizon": 8,
+            "input_shapes": {
+                "observation.image": [3, 96, 96],
+                "observation.state": [0],
+            },
+            "output_shapes": {
+                "action": [7],
+            },
+        }
     )
+    diff_cfg = DiffusionConfig(**diff_cfg)
+
+    cfg["diff_cfg"] = diff_cfg
 
     stats_path = os.path.join(cfg.root, "training/dataset_stats.pkl")
     if os.path.exists(stats_path):
@@ -191,6 +159,11 @@ def main(args):
         # Save stats
         pickle.dump(train_stats, open(stats_path, "wb"))
 
+    cfg["stats_path"] = stats_path
+    # Save cfg
+    with open(os.path.join(results_folder, "data_config.yaml"), "w") as file:
+        file.write(OmegaConf.to_yaml(cfg))
+
     policy = DiffusionPolicy(diff_cfg, dataset_stats=train_stats)
     policy.train()
     policy.to(device)
@@ -213,7 +186,6 @@ def main(args):
     while not done:
         for batch in dataloader:
             batch = {k: v.to(device, non_blocking=True) for k, v in batch.items()}
-            # breakpoint()
             output_dict = policy.forward(batch)
             loss = output_dict["loss"]
             loss.backward()
@@ -222,45 +194,30 @@ def main(args):
 
             if step % log_freq == 0:
                 print(f"step: {step} loss: {loss.item():.3f}")
+
+            if step % cfg.save_every == 0:
+                # Save model
+                saving_path = os.path.join(
+                    results_folder, f"model-{step // cfg.save_every}.pt"
+                )
+                torch.save(policy.state_dict(), saving_path)
             step += 1
             if step >= training_steps:
                 done = True
                 break
 
-    # Save a policy checkpoint.
-    policy.save_pretrained(output_directory)
-
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
+        "-s", "--server", type=str, default="hacienda"
+    )  # set to 'jz' to run on jean zay server
+    parser.add_argument(
         "-o", "--override", type=bool, default=False
     )  # set to True to overwrite results folder
     parser.add_argument(
-        "-num", "--num_samples", type=int, default=1
-    )  # set to number of samples to generate
-    parser.add_argument(
-        "-m", "--mode", type=str, default="train", choices=["train", "inference"]
-    )  # set to 'inference' to generate samples
-    parser.add_argument(
         "-c", "--checkpoint_num", type=int, default=None
     )  # set to checkpoint number to resume training or generate samples
-    parser.add_argument(
-        "-p", "--inference_path", type=str, default=None
-    )  # set to path to generate samples
-    parser.add_argument(
-        "-t", "--text", type=str, default=None
-    )  # set to text to generate samples
-    parser.add_argument(
-        "-n", "--sample_steps", type=int, default=100
-    )  # set to number of steps to sample
-    parser.add_argument(
-        "-g", "--guidance_weight", type=int, default=0
-    )  # set to positive to use guidance
+
     args = parser.parse_args()
-    if args.mode == "inference":
-        assert args.checkpoint_num is not None
-        assert args.inference_path is not None
-        assert args.text is not None
-        assert args.sample_steps <= 100
     main(args)
