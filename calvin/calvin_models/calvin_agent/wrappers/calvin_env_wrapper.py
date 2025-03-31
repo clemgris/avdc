644bf50c5ad11d9fd35c98afab0ffa5c4aaf87ec
clemgris <clemence.grislain@eleves.enpc.fr>
2025-03-31 10:32:30 +0200

    Explicit path

--- calvin/calvin_models/calvin_agent/wrappers/calvin_env_wrapper.py
+++ calvin/calvin_models/calvin_agent/wrappers/calvin_env_wrapper.py
@@ -2,13 +2,20 @@ import logging
 import os
 from typing import Any, Dict, Tuple, Union
 
-from calvin_agent.datasets.utils.episode_utils import process_depth, process_rgb, process_state
 import gym
 import numpy as np
 import torch
+from calvin_agent.datasets.utils.episode_utils import (
+    process_depth,
+    process_rgb,
+    process_state,
+)
 
-from calvin_env.envs.play_table_env import get_env
-from calvin_env.utils.utils import EglDeviceNotFoundError, get_egl_device_id
+from calvin.calvin_env.calvin_env.envs.play_table_env import get_env
+from calvin.calvin_env.calvin_env.utils.utils import (
+    EglDeviceNotFoundError,
+    get_egl_device_id,
+)
 
 logger = logging.getLogger(__name__)
 
@@ -17,7 +24,10 @@ class CalvinEnvWrapper(gym.Wrapper):
     def __init__(self, dataset_loader, device, show_gui=False, **kwargs):
         self.set_egl_device(device)
         env = get_env(
-            dataset_loader.abs_datasets_dir, show_gui=show_gui, obs_space=dataset_loader.observation_space, **kwargs
+            dataset_loader.abs_datasets_dir,
+            show_gui=show_gui,
+            obs_space=dataset_loader.observation_space,
+            **kwargs,
         )
         super(CalvinEnvWrapper, self).__init__(env)
         self.observation_space_keys = dataset_loader.observation_space
@@ -30,7 +40,9 @@ class CalvinEnvWrapper(gym.Wrapper):
     @staticmethod
     def set_egl_device(device):
         if "EGL_VISIBLE_DEVICES" in os.environ:
-            logger.warning("Environment variable EGL_VISIBLE_DEVICES is already set. Is this intended?")
+            logger.warning(
+                "Environment variable EGL_VISIBLE_DEVICES is already set. Is this intended?"
+            )
         cuda_id = device.index if device.type == "cuda" else 0
         try:
             egl_id = get_egl_device_id(cuda_id)
@@ -44,14 +56,36 @@ class CalvinEnvWrapper(gym.Wrapper):
         os.environ["EGL_VISIBLE_DEVICES"] = str(egl_id)
         logger.info(f"EGL_DEVICE_ID {egl_id} <==> CUDA_DEVICE_ID {cuda_id}")
 
-    def transform_observation(self, obs: Dict[str, Any]) -> Dict[str, Union[torch.Tensor, Dict[str, torch.Tensor]]]:
-        state_obs = process_state(obs, self.observation_space_keys, self.transforms, self.proprio_state)
-        rgb_obs = process_rgb(obs["rgb_obs"], self.observation_space_keys, self.transforms)
-        depth_obs = process_depth(obs["depth_obs"], self.observation_space_keys, self.transforms)
+    def transform_observation(
+        self, obs: Dict[str, Any]
+    ) -> Dict[str, Union[torch.Tensor, Dict[str, torch.Tensor]]]:
+        state_obs = process_state(
+            obs, self.observation_space_keys, self.transforms, self.proprio_state
+        )
+        rgb_obs = process_rgb(
+            obs["rgb_obs"], self.observation_space_keys, self.transforms
+        )
+        depth_obs = process_depth(
+            obs["depth_obs"], self.observation_space_keys, self.transforms
+        )
 
         state_obs["robot_obs"] = state_obs["robot_obs"].to(self.device).unsqueeze(0)
-        rgb_obs.update({"rgb_obs": {k: v.to(self.device).unsqueeze(0) for k, v in rgb_obs["rgb_obs"].items()}})
-        depth_obs.update({"depth_obs": {k: v.to(self.device).unsqueeze(0) for k, v in depth_obs["depth_obs"].items()}})
+        rgb_obs.update(
+            {
+                "rgb_obs": {
+                    k: v.to(self.device).unsqueeze(0)
+                    for k, v in rgb_obs["rgb_obs"].items()
+                }
+            }
+        )
+        depth_obs.update(
+            {
+                "depth_obs": {
+                    k: v.to(self.device).unsqueeze(0)
+                    for k, v in depth_obs["depth_obs"].items()
+                }
+            }
+        )
 
         obs_dict: Dict = {
             **rgb_obs,
@@ -63,7 +97,9 @@ class CalvinEnvWrapper(gym.Wrapper):
 
     def step(
         self, action_tensor: torch.Tensor
-    ) -> Tuple[Dict[str, Union[torch.Tensor, Dict[str, torch.Tensor]]], int, bool, Dict]:
+    ) -> Tuple[
+        Dict[str, Union[torch.Tensor, Dict[str, torch.Tensor]]], int, bool, Dict
+    ]:
         if self.relative_actions:
             action = action_tensor.squeeze().cpu().detach().numpy()
             assert len(action) == 7
@@ -73,7 +109,9 @@ class CalvinEnvWrapper(gym.Wrapper):
             elif action_tensor.shape[-1] == 8:
                 slice_ids = [3, 7]
             else:
-                logger.error("actions are required to have length 8 (for euler angles) or 9 (for quaternions)")
+                logger.error(
+                    "actions are required to have length 8 (for euler angles) or 9 (for quaternions)"
+                )
                 raise NotImplementedError
             action = np.split(action_tensor.squeeze().cpu().detach().numpy(), slice_ids)
         action[-1] = 1 if action[-1] > 0 else -1
