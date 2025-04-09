8b48211fca7e95f445e3c44d7ecff6f63f9632e5
clemgris <clemence.grislain@eleves.enpc.fr>
2025-04-09 11:31:08 +0200

    Icrease batch size

--- flowdiffusion/train/train_calvin.py
+++ flowdiffusion/train/train_calvin.py
@@ -49,7 +49,7 @@ def main(args):
                     "_target_": "calvin_agent.datasets.disk_dataset.DiskDiffusionDataset",
                     "key": "lang",
                     "save_format": "npz",
-                    "batch_size": 4,  # 8,
+                    "batch_size": 16,
                     "min_window_size": 16,
                     "max_window_size": 65,
                     "proprio_state": {
