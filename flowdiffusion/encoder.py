52d201b8bd586f43d3d5953e8144880808341294
clemgris <clemence.grislain@eleves.enpc.fr>
2025-04-17 18:10:47 +0200

    VIT dino feature extractor

--- flowdiffusion/encoder.py
+++ flowdiffusion/encoder.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
+from einops import rearrange
 from transformers import AutoImageProcessor, AutoModel
+from vit_wrapper import PretrainedViTWrapper
 
 
 class DinoV2Encoder(nn.Module):
@@ -19,3 +21,17 @@ class DinoV2Encoder(nn.Module):
         cls_emb = last_hidden_states[:, 0, :]
         patch_emb = last_hidden_states[:, 1:, :]
         return cls_emb, patch_emb
+
+
+class ViTEncoder(nn.Module):
+    def __init__(
+        self, name="vit_base_patch14_dinov2.lvd142m", image_size=224, device="cuda"
+    ):
+        super().__init__()
+        self.backbone = PretrainedViTWrapper(name=name, norm=True)
+        self.backbone = self.backbone.to(device)
+
+    def forward(self, x):
+        patch_emb = self.backbone(x).detach()
+        patch_emb = rearrange(patch_emb, "b c h w -> b (h w) c")
+        return None, patch_emb
